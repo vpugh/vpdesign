4df6b61d3350993c864e5d119da3f10d70a2d8c0
vpugh <hello@toripugh.com>
2020-12-21 02:13:22 -0500

    Remove manifest from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,18 +30,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/tp-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     {
